c2339de76bac682f7b348b160c9919db73de03e3
T_3k <lazoviy@mail.ru>
2022-02-06 03:56:22 +0300

    Поиск максимально возможного аудио битрейта в видео

--- services/ConverterService.py
+++ services/ConverterService.py
@@ -1,6 +1,7 @@
-from typing import Optional
 from moviepy.audio.AudioClip import CompositeAudioClip, AudioClip
+from typing import Optional
 import moviepy.editor
+import ffmpeg
 
 from bot_types.AudioConvertSettings import AudioConvertSettings
 from config import DIRS
@@ -8,6 +9,14 @@ from config import DIRS
 class ConverterService(object):
     @staticmethod
     def convert_video_to_audio(video_file_name: str, audio_convert_settings: Optional[AudioConvertSettings] = None) -> str:
+        video_file_info = ffmpeg.probe(DIRS['videos'] + '/' + video_file_name)
+
+        bit_rate = None
+        if video_file_info.get('streams') \
+            and len(video_file_info.get('streams')) > 1 \
+            and video_file_info.get('streams')[1].get('bit_rate'):
+            bit_rate = video_file_info.get('streams')[1].get('bit_rate')
+
         if audio_convert_settings:
             try:
                 with moviepy.editor.VideoFileClip(DIRS['videos'] + '/' + video_file_name) as video_clip:
@@ -23,7 +32,7 @@ class ConverterService(object):
                         
                         audio_clip = CompositeAudioClip([audio_clip, silence])
 
-                    audio_clip.write_audiofile(DIRS['audios'] + '/' + video_file_name[:-3] + 'mp3', audio_clip_fps)
+                    audio_clip.write_audiofile(DIRS['audios'] + '/' + video_file_name[:-3] + 'mp3', audio_clip_fps, bitrate=bit_rate)
             except Exception as error:
                 raise error
                 
